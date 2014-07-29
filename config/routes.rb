4bddcb8697393d1c736afed6d8391fffccbe7e1b
Ascariel <pablocangas@gmail.com>
2014-07-29 14:30:16 -0400

    active admin working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root to: 'visitors#index'
   get 'products/:id', to: 'products#show', :as => :products
   devise_for :users
